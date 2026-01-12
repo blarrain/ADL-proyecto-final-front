def33c28c0544c2cf5e71e9a5ec624846be2ee7b
blarrain <blarrain2@uc.cl>
2026-01-12 12:31:28 -0300

    silenciar notificaciones de deprecacion sass

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@scss': path.resolve(__dirname, './src/assets/scss'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: [
-          'import',
-          'mixed-decls',
-          'color-functions',
-          'global-builtin',
-        ],
-      },
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@scss': path.resolve(__dirname, './src/assets/scss'),
+			'@img': path.resolve(__dirname, './src/assets/img'),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				silenceDeprecations: [
+					'import',
+					'mixed-decls',
+					'color-functions',
+					'global-builtin',
+				],
+			},
+		},
+	},
+	server: {
+		port: 8080,
+	},
+});
