9f03c635ab9ba7cb89374875135f4bef345d69c8
blarrain <blarrain2@uc.cl>
2026-01-16 13:51:46 -0300

    token de inicio de sesión persistente

--- src/App.jsx
+++ src/App.jsx
@@ -12,34 +12,59 @@ import CartPage from './pages/CartPage';
 import ArticlePage from './pages/ArticlePage';
 import Footer from './components/Footer';
 
-
 import DetailArticle from './pages/DetailArticle';
 import FavoritesPages from './pages/FavoritesPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-  const { user } = useContext(UserContext)
+	// const { user } = useContext(UserContext)
+	const { token } = useContext(UserContext);
 
-  return (
-    <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/profile" element={user ? <ProfilePage /> : <Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/store" element={user ? <StorePage /> : <Navigate to="/login" />} />
-        <Route path="/cart" element={user ? <CartPage /> : <Navigate to="/login" />} />
-        <Route path="/article" element={user ? <ArticlePage /> : <Navigate to="/login" />} />
-        <Route path="/favorite" element={user ? <NotFoundPage /> : <Navigate to="/login" />} />
-        {/* <Route path="/favorite" element={user ? <FavoritesPages /> : <Navigate to="/login" />} /> */}z
-        <Route path="/detail/:id" element={user ? <DetailArticle /> : <Navigate to="/login" />} />
-        <Route path="*" element={<NotFoundPage />} /> {/* /404 */}
-      </Routes>
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				{/* <Route path="/profile" element={user ? <ProfilePage /> : <Navigate to="/login" />} /> */}
+				<Route
+					path='/profile'
+					element={token ? <ProfilePage /> : <Navigate to='/login' />}
+				/>
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/register' element={<RegisterPage />} />
+				{/* <Route path="/store" element={user ? <StorePage /> : <Navigate to="/login" />} /> */}
+				<Route
+					path='/store'
+					element={token ? <StorePage /> : <Navigate to='/login' />}
+				/>
+				{/* <Route path="/cart" element={user ? <CartPage /> : <Navigate to="/login" />} /> */}
+				<Route
+					path='/cart'
+					element={token ? <CartPage /> : <Navigate to='/login' />}
+				/>
+				{/* <Route path="/article" element={user ? <ArticlePage /> : <Navigate to="/login" />} /> */}
+				<Route
+					path='/article'
+					element={token ? <ArticlePage /> : <Navigate to='/login' />}
+				/>
+				{/* <Route path="/favorite" element={user ? <NotFoundPage /> : <Navigate to="/login" />} /> */}
+				<Route
+					path='/favorite'
+					element={token ? <NotFoundPage /> : <Navigate to='/login' />}
+				/>
+				{/* <Route path="/favorite" element={user ? <FavoritesPages /> : <Navigate to="/login" />} /> */}
+				z
+				{/* <Route path="/detail/:id" element={user ? <DetailArticle /> : <Navigate to="/login" />} /> */}
+				<Route
+					path='/detail/:id'
+					element={token ? <DetailArticle /> : <Navigate to='/login' />}
+				/>
+				<Route path='*' element={<NotFoundPage />} /> {/* /404 */}
+			</Routes>
 
-      <Footer />
-    </div>
-  );
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,48 +1,64 @@
-import { createContext, useState } from "react";
-import { users } from "../assets/data/users";
-import { useNavigate } from "react-router-dom";
+import { createContext, useState } from 'react';
+import { users } from '../assets/data/users';
+import { useNavigate } from 'react-router-dom';
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-    const [token, setToken] = useState(false)
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [user, setUser] = useState(null)
-    const [msgError, setMsgError] = useState("");
-    const navigate = useNavigate()
+	// const [token, setToken] = useState(false)
+	const [token, setToken] = useState(localStorage.getItem('token'));
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [user, setUser] = useState(null);
+	const [msgError, setMsgError] = useState('');
+	const navigate = useNavigate();
 
-    const logout = () => {
-        setToken(false)
-        setUser(null)
-    }
+	const logout = () => {
+		// setToken(false);
+		setToken(null);
+		localStorage.removeItem('token');
+		setUser(null);
+	};
 
-    const login = async (e) => {
+	const login = async (e) => {
+		let userValid = false;
 
-        let userValid = false
+		for (let element of users) {
+			if (element.email == email && element.password == password) {
+				userValid = true;
+				setUser(element);
+				break;
+			}
+		}
 
-        for (let element of users) {
-            if (element.email == email && element.password == password) {
-                userValid = true
-                setUser(element)
-                break
-            }
-        };
+		if (!userValid) {
+			setMsgError('Error en las credenciales');
+			setToken(false);
+		} else {
+			setMsgError('');
+			setToken(true);
+			localStorage.setItem('token', true);
+			navigate(`/store`);
+		}
+	};
 
-        if (!userValid) {
-            setMsgError("Error en las credenciales")
-            setToken(false)
-        } else {
-            setMsgError("")
-            setToken(true)
-            navigate(`/store`)
-        }
-
-    }
-
-    return (
-        <UserContext.Provider value={{ token, email, password, user, setToken, setEmail, setPassword, logout, login, msgError, setMsgError }}>
-            {children}
-        </UserContext.Provider>
-    );
+	return (
+		<UserContext.Provider
+			value={{
+				token,
+				email,
+				password,
+				user,
+				setToken,
+				setEmail,
+				setPassword,
+				logout,
+				login,
+				msgError,
+				setMsgError,
+			}}
+		>
+			{children}
+		</UserContext.Provider>
+	);
 };
 export default UserProvider;
