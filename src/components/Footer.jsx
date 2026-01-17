e915a060c091aa3962346ac607bec8f95dcc06ff
blarrain <blarrain2@uc.cl>
2026-01-17 16:07:51 -0300

    corrección ancho interno footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className=" footer-vivero text-center mt-2 py-2 ">
-      <div className="row justify-content-center">
+    <footer className="footer-vivero text-center mt-2 py-2">
+      <div>
         <p>© 2026 - Vivero JRB - Todos los derechos reservados</p>
       </div>
     </footer>
